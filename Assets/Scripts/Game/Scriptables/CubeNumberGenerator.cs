6a7f82afcf95e0ae6bb73e7f8dbf197bdad849c4
owl.sifu <owl.sifu@gmail.com>
2025-07-06 18:08:19 +0300

    your new commit message

---
+++ Assets/Scripts/Game/Scriptables/CubeNumberGenerator.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Game.Scriptables
+{
+    [CreateAssetMenu(menuName = "Game/CubeNumberGenerator")]
+    class CubeNumberGenerator : ScriptableObject
+    {
+        [Header("Initial Spawn Settings")]
+        [SerializeField] private int _chanceToSpawnMinValue = 75;
+        [SerializeField] private int _minValue = 2;
+        [SerializeField] private int _maxValue = 4;
+
+        public int Generate()
+        {
+            return Random.value < (_chanceToSpawnMinValue / 100f) ? _minValue : _maxValue;
+        }
+
+        public int GenerateNext(int number)
+        {
+            return number * 2;
+        }
+
+        public int GetIndex(int number)
+        {
+            return GetPower(number) - 1;
+        }
+
+        public int GetNextPower(int number)
+        {
+            return GetPower(number) + 1;
+        }
+
+        public int GetPower(int number)
+        {
+            return (int)Mathf.Log(number, 2);
+        }
+
+        public int GetNumber(int power)
+        {
+            return (int)Mathf.Pow(2, power);
+        }
+    }
+}
